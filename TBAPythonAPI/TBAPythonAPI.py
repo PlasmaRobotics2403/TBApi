da238dfb843f74f6ef376976e53b1ea1bd81a3b0
A Detection Algorithm (ADA) <Adetectionalgorithmbot@gmail.com>
2016-06-21 22:33:24 -0700

    fix a lot of things which probably have no bearing

--- TBAPythonAPI/TBAPythonAPI.py
+++ TBAPythonAPI/TBAPythonAPI.py
@@ -12,65 +12,65 @@ class TBAParser:
 
     #Team Info
 
-    def get_team_obj(self, teamNumber):
+    def get_team_obj(self, team_number):
 
-        request = (self.baseURL + "/team/frc" + teamNumber)
-        response = requests.get(myRequest, headers=header)
+        request = (self.baseURL + "/team/frc" + str(team_number))
+        response = requests.get(request, headers=header)
         dictionary = response.json()
 
         return dictionary
 
-    def get_team_full_name(self, teamNumber):
-        team_dictionary = get_team_obj(teamNumber)
+    def get_team_full_name(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         full_name = team_dictionary['name']
         return full_name
 
-    def get_team_nick_name(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_nick_name(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         nick_name = team_dictionary['nickname']
         return nick_name
 
-    def get_team_number(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_number(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         team_number = team_dictionary['team_number']
         return team_number
 
-    def get_team_rookie_year(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_rookie_year(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         rookie_year = team_dictionary['rookie_year']
         return rookie_year
 
-    def get_team_website(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
-        website = team_dictionary['rookie_year']
+    def get_team_website(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        website = team_dictionary['website']
         return website
 
-    def get_team_city(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_city(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         city = team_dictionary['locality']
         return city
 
-    def get_team_region(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_region(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         region = team_dictionary['region']
         return region
 
-    def get_team_location(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_location(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         location = team_dictionary['location']
         return location
 
-    def get_team_key(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_key(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         key = team_dictionary['key']
         return key
 
-    def get_team_country(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_country(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         country = team_dictionary['country_name']
         return country
 
-    def get_team_motto(self, teamNumber):
-        team_dictionary = getTeamObj(teamNumber)
+    def get_team_motto(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
         motto = team_dictionary['motto']
         return motto
