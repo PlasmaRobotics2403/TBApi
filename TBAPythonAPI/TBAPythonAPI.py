11d3a2cf34f3afc9a1791190889dae699939a590
A Detection Algorithm (ADA) <Adetectionalgorithmbot@gmail.com>
2016-06-21 20:23:56 -0700

    Added basic __init__.py script

--- TBAPythonAPI/TBAPythonAPI.py
+++ TBAPythonAPI/TBAPythonAPI.py
@@ -1,1 +1,11 @@
+import os
+import sys
+import requests
 
+class TBAParser:
+  def __init__(self, teamNumber, packageID, versionID)
+    self.teamNumber = teamNumber
+    self.packageID = packageID
+    self.versionID = versionID
+    self.header = {'X-TBA-App-Id': 'frc{team}:{package}:{version}'.format(team = teamNumber, package = packageID, version = versionID)}
+    self.baseURL = 'http://www.thebluealliance.com/api/v2/'
