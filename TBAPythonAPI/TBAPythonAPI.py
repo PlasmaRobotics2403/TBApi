517ff9fb177662a774af2d693f2325ce07d23b60
A Detection Algorithm (ADA) <Adetectionalgorithmbot@gmail.com>
2016-06-22 16:38:56 -0700

    Added basic class setup
    Next-project: add individual variables that pull from self.raw

--- TBAPythonAPI/TBAPythonAPI.py
+++ TBAPythonAPI/TBAPythonAPI.py
@@ -1,14 +1,26 @@
 class TBA_Team:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBA_Event:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBA_Match:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBA_Award:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBA_Media:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBA_Robot:
+    def __init__(self, rawJSON):
+        self.raw = rawJSON
 
 class TBAParser:
     def __init__(self, teamNumber, packageID, versionID):
