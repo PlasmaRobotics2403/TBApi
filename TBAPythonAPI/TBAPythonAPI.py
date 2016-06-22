f5684ff94ee8ef499b665259f06688dd12692d1b
nardavin <nardavin@gmail.com>
2016-06-21 22:39:31 -0700

    Steps to merge

--- TBAPythonAPI/TBAPythonAPI.py
+++ TBAPythonAPI/TBAPythonAPI.py
@@ -31,6 +31,51 @@ class TBAParser:
         nick_name = team_dictionary['nickname']
         return nick_name
 
+    def get_team_number(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        team_number = team_dictionary['team_number']
+        return team_number
+
+    def get_team_rookie_year(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        rookie_year = team_dictionary['rookie_year']
+        return rookie_year
+
+    def get_team_website(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        website = team_dictionary['website']
+        return website
+
+    def get_team_city(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        city = team_dictionary['locality']
+        return city
+
+    def get_team_region(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        region = team_dictionary['region']
+        return region
+
+    def get_team_location(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        location = team_dictionary['location']
+        return location
+
+    def get_team_key(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        key = team_dictionary['key']
+        return key
+
+    def get_team_country(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        country = team_dictionary['country_name']
+        return country
+
+    def get_team_motto(self, team_number):
+        team_dictionary = self.get_team_obj(team_number)
+        motto = team_dictionary['motto']
+        return motto
+
 
     #Event Info
 
---
+++ test.py
@@ -0,0 +1,6 @@
+
+from TBAPythonAPI import *
+
+data = TBAParser(2403, "tester", 1)
+print("test")
+print(data.get_team_website(2403))
